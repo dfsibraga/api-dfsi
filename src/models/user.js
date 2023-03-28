0c2b2602a43d491a71285c14101e93be1cbc485e
aml <amadeu@dfsi.pt>
2023-03-28 21:26:37 +0100

    password hashed

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     codigo: {
@@ -20,7 +21,10 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function (next) {
     const user = this;
 
-    
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    };
+
     next();
 });
 
