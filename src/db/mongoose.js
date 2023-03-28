39609f2e0287356cf4c04e2ecb5e152974debfb2
aml <amadeu@dfsi.pt>
2023-03-28 21:51:31 +0100

    password hashed

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI, {
     dbName: "demo",
     useNewUrlParser:true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex : true
 });
-
 
